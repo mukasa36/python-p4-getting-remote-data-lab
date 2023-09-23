790ad6cd4fd9e8922e3a0206d42de056e1380445
emmanuel <emmanuel.mukasa@moringaschool.com>
2023-09-23 10:26:09 +0300

    almost done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,24 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        if response.status_code == 200:
+            return response.text
+        else:
+
+            raise Exception(f"Request failed with status code {response.status_code}")
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        try:
+            data = json.loads(response_body)
+            return data
+        except json.JSONDecodeError as e:
+        
+            raise Exception(f"Failed to parse JSON response: {str(e)}")
+
+
+url = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+requester = GetRequester(url)
+data = requester.load_json()
+print(data)
